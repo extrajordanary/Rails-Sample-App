18a14747d700f0d5a13b329b68467dc7202c7ba1
Jordan Arnesen <jordan101112@gmail.com>
2014-12-04 17:08:07 -0800

    Add User signup and display page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  get 'user/new'
+  get 'user/new' # old?
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
